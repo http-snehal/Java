9a83815578937fc329d8f72c5cbb9181eeb5a749
http-snehal <snehalkushwah24@gmail.com>
2025-06-27 04:31:09 +0530

    calculator done

--- calculator/calculator.java
+++ calculator/calculator.java
@@ -10,13 +10,46 @@ public class calculator {
         int choice ;
 
 
-        System.out.println("enter the number 1: ");
+        System.out.print("enter the number 1: ");
         num1 = sc.nextDouble();
 
-        System.out.println("Enter the number 2: ");
+        System.out.print("Enter the number 2: ");
         num2 = sc.nextDouble();
 
+        System.out.println(" ");
+
+        System.out.println("choose operation \n1. + \n2. - \n3 .* \n4. / \n5. % \n6.^");
         
+
+        System.out.print("enter the choice[1-6]: ");
+        choice = sc.nextInt();
+        double result = 0 ;
+
+        switch(choice){
+            case 1 -> result = num1 + num2;
+            case 2 -> result = num1 - num2;
+            case 3 -> result = num1 * num2;
+            case 4 ->{
+             if(num2 == 0) {
+                System.out.println("cant divide it with 0");
+
+            }
+            else{
+                result = num1/num2;
+
+            } 
+        }
+        case 5 -> result = num1 % num2;
+        case 6 -> result = Math.pow(num1, num2);
+
+            default -> System.out.println("invalid choice");
+
+
+        }
+
+        System.out.println(result);
+
+
 
 
 
